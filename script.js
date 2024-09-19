8586bb324ccfb3efbf21d37381939a38609799cb
Veronica Souza <codebyveronica@gmail.com>
2024-09-19 14:36:14 -0300

    client side form validation with js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const form = document.querySelector("#form");
+const btn = document.querySelector("button");
 const firstName = document.querySelector("#fname");
 const lastName = document.querySelector("#lname");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 
-form.addEventListener("submit", (e) => {
+btn.addEventListener("click", (e) => {
    e.preventDefault();
 
    checkInputs();
@@ -18,6 +18,28 @@ function checkInputs() {
 
     if (fNameValue === '') {
         setErrorFor(firstName, "First name cannot be empty")
+    } else {
+        setSucess(firstName);
+    }
+
+    if (lNameValue === '') {
+        setErrorFor(lastName, "Last name cannot be empty");
+    } else {
+        setSucess(lastName);
+    }
+
+    if(emailValue === '') {
+        setErrorFor(email, "Email cannot be empty");
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, "Not a valid email");
+    } else {
+        setSucess(email);
+    }
+
+    if(passwordValue === '') {
+        setErrorFor(password, "Password cannot be empty");
+    } else {
+        setSucess(password);
     }
 }
 
@@ -28,4 +50,14 @@ function setErrorFor(input, message) {
     error.innerText = message;
 
     field.className = "field error";
+}
+
+function setSucess(input) {
+    const field = input.parentElement;
+
+    field.className = "field";
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
